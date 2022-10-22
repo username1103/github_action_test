c9b4d7e4f27c93e8370452a46edf44188ef04db9
myeongil <username1103@naver.com>
2022-10-23 01:58:45 +0900

    fix: 실패 테스트 케이스 제거

--- test/test.test.ts
+++ test/test.test.ts
@@ -12,18 +12,6 @@ test("1+1 = 2", async () => {
   expect(result).toEqual(2);
 });
 
-test("실패 테스트 1+1 = 3", async () => {
-  // given
-  const a = 1;
-  const b = 1;
-
-  // when
-  const result = sum(a, b);
-
-  // then
-  expect(result).toEqual(3);
-});
-
 test("env check", async () => {
   // given
 
