ecd6081eab1bdf520b371e77fb8dfe4164ca8077
myeongil <username1103@naver.com>
2022-10-23 02:25:48 +0900

    feat: 콘솔 제거

--- test/test.test.ts
+++ test/test.test.ts
@@ -15,13 +15,6 @@ test("1+1 = 2", async () => {
 
 test("env check", async () => {
   // given
-  console.log({
-    host: process.env.MYSQL_HOSTNAME,
-    port: parseInt(process.env.MYSQL_PORT, 10),
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  });
   const conn = await mysql2.createConnection({
     host: process.env.MYSQL_HOSTNAME,
     port: parseInt(process.env.MYSQL_PORT, 10),
