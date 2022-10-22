2de52f677e5c272e3191d0c279c1f65307d82325
myeongil <username1103@naver.com>
2022-10-23 01:39:30 +0900

    test: 실패 테스트 케이스 추가

--- test/test.test.ts
+++ test/test.test.ts
@@ -11,3 +11,15 @@ test("1+1 = 2", async () => {
   // then
   expect(result).toEqual(2);
 });
+
+test("실패 테스트 1+1 = 3", async () => {
+  // given
+  const a = 1;
+  const b = 1;
+
+  // when
+  const result = sum(a, b);
+
+  // then
+  expect(result).toEqual(3);
+});
